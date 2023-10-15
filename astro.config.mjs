fcd3bcf8219832c5379e760c6e97a5e32c7f6d55
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-10-15 19:18:39 -0300

    Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push']
+      }
+    })
+  ]
 });
